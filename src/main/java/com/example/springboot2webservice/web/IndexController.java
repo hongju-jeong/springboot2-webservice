e2df0d7fe86e80e9f1b5604960799c86328419e1
HongJu Jeong <sub06038@naver.com>
2022-08-28 00:12:49 +0900

    spring boot security with OAuth2

--- src/main/java/com/example/springboot2webservice/web/IndexController.java
+++ src/main/java/com/example/springboot2webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springboot2webservice.web;
 
+import com.example.springboot2webservice.config.auth.dto.SessionUser;
 import com.example.springboot2webservice.service.posts.PostsService;
 import com.example.springboot2webservice.web.dto.PostsResponseDto;
 import com.sun.org.apache.xpath.internal.operations.Mod;
@@ -9,16 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
